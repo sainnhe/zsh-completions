67c88f3399e3360bb5dc1fa4d3a8a2c2a3ff9cbb
Sainnhepark <sainnhe@gmail.com>
2020-04-12 16:26:47 +0800

    adjust order

--- zsh-completions.plugin.zsh
+++ zsh-completions.plugin.zsh
@@ -1,4 +1,4 @@
-fpath+="${0:h}/src/custom"
+source "${0:h}/src/MenkeTechnologies.zsh-more-completions/zsh-more-completions.plugin.zsh"
 source "${0:h}/src/zsh-users.zsh-completions/zsh-completions.plugin.zsh"
 source "${0:h}/src/zchee.zsh-completions/zsh-completions.plugin.zsh"
 __yeoman_pluginDir="$(dirname "$0")"/src/edouard-lopez.yeoman-zsh-plugin
@@ -7,5 +7,5 @@ export __yeoman_pluginDir
 (__yo_setGlobalGenerators &)
 source "${0:h}/src/gradle.gradle-completion/gradle-completion.plugin.zsh"
 source "${0:h}/src/esc.conda-zsh-completion/conda-zsh-completion.plugin.zsh"
-source "${0:h}/src/MenkeTechnologies.zsh-more-completions/zsh-more-completions.plugin.zsh"
+fpath+="${0:h}/src/custom"
 
