c080294e2ad819bd5d2505b4d452b92366e5ff1b
Sainnhepark <sainnhe@gmail.com>
2021-02-04 12:32:44 +0800

    remove pip completions from OMZ

--- src/custom/gen.sh
+++ src/custom/gen.sh
@@ -4,5 +4,4 @@ pip completion --zsh > _pip
 npm completion > _npm
 rustup completions zsh rustup > _rustup
 rustup completions zsh cargo > _cargo
-proxychains -q wget https://github.com/ohmyzsh/ohmyzsh/raw/master/plugins/pip/_pip
 
