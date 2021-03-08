7689bd410476f8a9c46de708bd2980bd54e0267a
Sainnhe Park <sainnhe@gmail.com>
2021-03-08 11:37:11 +0800

    load completions on demand

--- zsh-completions.plugin.zsh
+++ zsh-completions.plugin.zsh
@@ -1,8 +1,8 @@
 source "${0:h}/src/zsh-users.zsh-completions/zsh-completions.plugin.zsh"
 source "${0:h}/src/zchee.zsh-completions/zsh-completions.plugin.zsh"
-source "${0:h}/src/gradle.gradle-completion/gradle-completion.plugin.zsh"
-source "${0:h}/src/esc.conda-zsh-completion/conda-zsh-completion.plugin.zsh"
-source "${0:h}/src/custom/npm.plugin.zsh"
-source "${0:h}/src/custom/pip.plugin.zsh"
+[ -x "$(command -v gradle)" ] && source "${0:h}/src/gradle.gradle-completion/gradle-completion.plugin.zsh"
+[ -x "$(command -v conda)" ] && source "${0:h}/src/esc.conda-zsh-completion/conda-zsh-completion.plugin.zsh"
+[ -x "$(command -v npm)" ] && source "${0:h}/src/custom/npm.plugin.zsh"
+[ -x "$(command -v pip)" ] && source "${0:h}/src/custom/pip.plugin.zsh"
 fpath+="${0:h}/src/custom"
 
