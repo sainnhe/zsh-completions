43cf148fbca986453c4353d415fed32c4f0bdbfc
Sainnhe Park <sainnhe@gmail.com>
2021-09-19 17:46:09 +0800

    Remove azure completion definitions

--- src/custom/gen.sh
+++ src/custom/gen.sh
@@ -3,7 +3,6 @@
 cd $(git rev-parse --show-toplevel)/src/custom
 pip completion --zsh > pip.plugin.zsh
 npm completion > npm.plugin.zsh
-curl -L https://raw.githubusercontent.com/Azure/azure-cli/dev/az.completion > az.plugin.zsh
 rustup completions zsh rustup > _rustup
 rustup completions zsh cargo > _cargo
 
