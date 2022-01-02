0ae0a3e3afd0eb3318feee59046c716fa3fa015c
Sainnhe Park <sainnhe@gmail.com>
2022-01-02 10:36:17 +0800

    Add _limactl

--- src/custom/gen.sh
+++ src/custom/gen.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 
 cd $(git rev-parse --show-toplevel)/src/custom
-pip completion --zsh > pip.plugin.zsh
-npm completion > npm.plugin.zsh
-rustup completions zsh rustup > _rustup
-rustup completions zsh cargo > _cargo
+[ -x "$(command -v pip)" ] && pip completion --zsh > pip.plugin.zsh
+[ -x "$(command -v npm)" ] && npm completion > npm.plugin.zsh
+[ -x "$(command -v rustup)" ] && rustup completions zsh rustup > _rustup
+[ -x "$(command -v rustup)" ] && rustup completions zsh cargo > _cargo
+[ -x "$(command -v limactl)" ] && limactl completion zsh > _limactl
 
