028089e2fee5b64bef5f6965f1f9069f0a2fd92a
Sainnhe Park <sainnhe@gmail.com>
2022-02-01 13:41:35 +0800

    Finish update.zsh

--- src/custom/gen.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-cd $(git rev-parse --show-toplevel)/src/custom
-[ -x "$(command -v pip)" ] && pip completion --zsh > pip.plugin.zsh
-[ -x "$(command -v npm)" ] && npm completion > npm.plugin.zsh
-[ -x "$(command -v rustup)" ] && rustup completions zsh rustup > _rustup
-[ -x "$(command -v rustup)" ] && rustup completions zsh cargo > _cargo
-[ -x "$(command -v limactl)" ] && limactl completion zsh > _limactl
-curl -L https://github.com/docker/cli/raw/master/contrib/completion/zsh/_docker > _docker
