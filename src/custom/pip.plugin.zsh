37ff6b904db5017fa2d8ac053e0df1dbb47b56eb
Sainnhe Park <i@sainnhe.dev>
2023-09-02 10:45:08 +0800

    Update custom completions

--- src/custom/pip.plugin.zsh
+++ src/custom/pip.plugin.zsh
@@ -1,13 +1,8 @@
 
 # pip zsh completion start
-function _pip_completion {
-  local words cword
-  read -Ac words
-  read -cn cword
-  reply=( $( COMP_WORDS="$words[*]" \
-             COMP_CWORD=$(( cword-1 )) \
-             PIP_AUTO_COMPLETE=1 $words[1] 2>/dev/null ))
-}
-compctl -K _pip_completion pip
+#compdef -P pip[0-9.]#
+compadd $( COMP_WORDS="$words[*]" \
+           COMP_CWORD=$((CURRENT-1)) \
+           PIP_AUTO_COMPLETE=1 $words[1] 2>/dev/null )
 # pip zsh completion end
 
