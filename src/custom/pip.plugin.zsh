134cc737ea18a49c795b334cdb15b4e01dc92afb
Sainnhe Park <i@sainnhe.dev>
2023-09-02 23:48:23 +0800

    Fix pip

--- src/custom/pip.plugin.zsh
+++ src/custom/pip.plugin.zsh
@@ -1,8 +1,9 @@
-
-# pip zsh completion start
-#compdef -P pip[0-9.]#
-compadd $( COMP_WORDS="$words[*]" \
-           COMP_CWORD=$((CURRENT-1)) \
-           PIP_AUTO_COMPLETE=1 $words[1] 2>/dev/null )
-# pip zsh completion end
-
+function _pip_completion {
+  local words cword
+  read -Ac words
+  read -cn cword
+  reply=( $( COMP_WORDS="$words[*]" \
+             COMP_CWORD=$(( cword-1 )) \
+             PIP_AUTO_COMPLETE=1 $words[1] 2>/dev/null ))
+}
+compctl -K _pip_completion pip
