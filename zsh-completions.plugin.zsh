f9588a3cafd996b99f40bd5772b42d137ea18cc2
Sainnhe Park <i@sainnhe.dev>
2025-03-28 23:09:41 +0800

    Fix pnpm completion

--- zsh-completions.plugin.zsh
+++ zsh-completions.plugin.zsh
@@ -4,7 +4,6 @@ source "${0:h}/src/zchee.zsh-completions/zsh-completions.plugin.zsh"
 [ -x "$(command -v gradle)" ] && source "${0:h}/src/gradle.gradle-completion/gradle-completion.plugin.zsh"
 [ -x "$(command -v conda)" ] && source "${0:h}/src/conda-incubator.conda-zsh-completion/conda-zsh-completion.plugin.zsh"
 [ -x "$(command -v npm)" ] && source "${0:h}/src/custom/npm.plugin.zsh"
-[ -x "$(command -v pnpm)" ] && source "${0:h}/src/custom/pnpm.plugin.zsh"
 [ -x "$(command -v pip)" ] && source "${0:h}/src/custom/pip.plugin.zsh"
 fpath+="${0:h}/src/custom"
 
